500697df00f578ec9d9afe907b17b6d5c540a2c0
Akila <akila271819@gmail.com>
2025-09-13 00:23:17 +0530

    initial commit

--- backend/settings.py
+++ backend/settings.py
@@ -90,17 +90,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import os
 import dj_database_url
+from decouple import config
 
 DATABASES = {
-    'default': dj_database_url.config(default=config("DATABASE_URL"))
+    'default': dj_database_url.config(
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}"
+    )
 }
+
 
 
 # Password validation
